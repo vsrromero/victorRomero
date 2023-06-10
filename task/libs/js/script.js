f91acc9773b947363f3204c0a7aa5e8efd43c613
Victor Romero <vsr.romero@gmail.com>
2023-06-10 13:32:06 +0100

    code maintenance

--- task/libs/js/script.js
+++ task/libs/js/script.js
@@ -33,37 +33,6 @@ $(document).ready(function() {
         }
     });
 
-    // // Event listener postalCodeSearch button
-    // $('#submit-btn-postalCode').click(function() {
-    //     var userInput = $('#user-input-postalCode').val();
-
-    //     if (userInput !== '') {
-    //         var url = 'libs/php/getPostalCodeSearch.php?postalcode=' + userInput;
-    //         var result = performAjaxRequest(url);
-
-    //         displayResult(result);
-    //     }
-    // });
-
-    // // Event listener findNearby button
-    // $('#submit-btn-findNearby').click(function() {
-    //     var userInput = $('#user-input-findNearby').val();
-
-    //     if (userInput !== '') {
-    //         var coords = userInput.split(',');
-
-    //         if (coords.length === 2) {
-    //             var lat = coords[0].trim();
-    //             var lng = coords[1].trim();
-
-    //             var url = 'libs/php/getFindNearby.php?lat=' + lat + '&lng=' + lng;
-
-    //             performAjaxRequest(url);
-    //         }
-    //     }
-    // });
-
-    // Função para executar a requisição AJAX
     function performAjaxRequest(url) {
         return new Promise(function(resolve, reject) {
             $.ajax({
@@ -79,17 +48,5 @@ $(document).ready(function() {
             });
         });
     }
-
-    // Função para exibir o resultado na página
-    // function displayResult(resultPromise) {
-    //     resultPromise
-    //         .then(function(result) {
-    //             console.log(result);
-    //             $('#result').text(JSON.stringify(result));
-    //         })
-    //         .catch(function(error) {
-    //             console.error(error);
-    //         });
-    // }
     
 });
