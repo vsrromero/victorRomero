bf61940308b3c7320700e34f125bf58faad87ec4
Victor Romero <vsr.romero@gmail.com>
2023-06-22 03:00:37 +0100

    Country leyers working

--- project1/assets/js/app.js
+++ project1/assets/js/app.js
@@ -5,23 +5,51 @@ var selectedCountryName = '';
 //* End of global variables
 
 //* general functions 
+/*
+!when using the function away from the methods of select change and geolocation in navigator, sometims the modals are not rendered, so I had to put the function inside the methods
+async function renderInfoModal(selectedCountry, selectedCountryCode) {
+}
+*/
+
+async function getCoordinatesFromCountry(countryName) {
+    try {
+        const response = await fetch('assets/php/openCage.php?countryName=' + countryName);
+        const data = await response.json();
+        const country = data.results[0].geometry;
+        return [country.lat, country.lng];
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
+}
+
+
 
 //! Check if a point is inside a polygon
 function pointInPolygon(latitude, longitude, coordinates) {
-    console.log('pointInPolygon');
     var isInPolygon = false;
 
-    for (var i = 0, j = coordinates.length - 1; i < coordinates.length; j = i++) {
-        var xi = coordinates[i][0][0];
-        var yi = coordinates[i][0][1];
-        var xj = coordinates[j][0][0];
-        var yj = coordinates[j][0][1];
+    for (var i = 0; i < coordinates.length; i++) {
+        var polygon = coordinates[i];
 
-        var intersect = ((yi > latitude) != (yj > latitude)) &&
-            (longitude < (xj - xi) * (latitude - yi) / (yj - yi) + xi);
+        for (var j = 0; j < polygon.length; j++) {
+            var ring = polygon[j];
 
-        if (intersect) {
-            isInPolygon = !isInPolygon;
+            var intersect = false;
+            for (var k = 0, l = ring.length - 1; k < ring.length; l = k++) {
+                var xi = ring[k][0];
+                var yi = ring[k][1];
+                var xj = ring[l][0];
+                var yj = ring[l][1];
+
+                if (((yi > latitude) !== (yj > latitude)) && (longitude < (xj - xi) * (latitude - yi) / (yj - yi) + xi)) {
+                    intersect = !intersect;
+                }
+            }
+
+            if (intersect) {
+                isInPolygon = !isInPolygon;
+            }
         }
     }
 
@@ -31,15 +59,44 @@ function pointInPolygon(latitude, longitude, coordinates) {
 //! Get the country name by latitude and longitude
 
 function getCountryFromCoordinates(latitude, longitude, geoJSON) {
-    console.log('getCountryFromCoordinates');
     for (var i = 0; i < geoJSON.features.length; i++) {
         var feature = geoJSON.features[i];
         if (pointInPolygon(latitude, longitude, feature.geometry.coordinates)) {
+            updateCountryList();
             return feature.properties.name;
         }
     }
     console.log('No country found');
     return ""; // Retorna uma string vazia se nenhum país for encontrado
+}
+
+function getCountryCodeFromCoordinates(latitude, longitude, geoJSON) {
+    for (var i = 0; i < geoJSON.features.length; i++) {
+        var feature = geoJSON.features[i];
+        if (pointInPolygon(latitude, longitude, feature.geometry.coordinates)) {
+            return feature.properties.iso_a2;
+        }
+    }
+    console.log('No country found');
+    return ""; // Retorna uma string vazia se nenhum país for encontrado
+}
+
+function updateCountryList() {
+    var selectElement = $('#countriesList select');
+
+    $.getJSON('assets/php/getCountryList.php', function (data) {
+        selectElement.empty(); // Limpa a lista antes de atualizá-la
+
+        $.each(data, function (index, country) {
+            var option = $('<option>').val(country.iso_a2).text(country.name);
+
+            if (country.name === selectedCountryName) {
+                option.attr('selected', 'selected');
+            }
+
+            selectElement.append(option);
+        });
+    });
 }
 
 //* End of general functions
@@ -57,8 +114,8 @@ var map = L.map('map').setView([51.505, -0.09], 15);
 
 // map layer
 var mapLayer = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-	maxZoom: 16,
-	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    maxZoom: 16,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 });
 
 mapLayer.addTo(map);
@@ -78,6 +135,49 @@ $.getJSON('assets/json/countryBorders.geo.json', function (data) {
 
 });
 
+// control buttons
+var markerControl = L.control();
+var infoControl = L.control();
+var weatherControl = L.control();
+
+// button html
+markerControl.onAdd = function (map) {
+    var button = L.DomUtil.create('button', 'leaflet-bar leaflet-control');
+    button.innerHTML = '<i class="fas fa-map-marker-alt"></i>';
+    button.title = 'Toggle Markers';
+    button.classList.add('control-button');
+    L.DomEvent.on(button, 'click', function () {
+        toggleMarkers();
+    });
+    return button;
+};
+
+infoControl.onAdd = function (map) {
+    var button = L.DomUtil.create('button', 'leaflet-bar leaflet-control');
+    button.innerHTML = '<i class="fas fa-info"></i>';
+    button.title = 'Show Info';
+    button.classList.add('control-button');
+    L.DomEvent.on(button, 'click', function () {
+        $('#modal').modal('show')
+    });
+    return button;
+};
+
+weatherControl.onAdd = function (map) {
+    var button = L.DomUtil.create('button', 'leaflet-bar leaflet-control');
+    button.innerHTML = '<i class="fas fa-cloud-sun"></i>';
+    button.title = 'Show Weather';
+    button.classList.add('control-button');
+    L.DomEvent.on(button, 'click', function () {
+        $('#weatherModal').modal('show')
+    });
+    return button;
+};
+
+// add controls to map
+markerControl.addTo(map);
+infoControl.addTo(map);
+weatherControl.addTo(map);
 
 //* End of Leaflet codes
 
@@ -96,13 +196,48 @@ if ("geolocation" in navigator) {
         L.marker([latitude, longitude]).addTo(map);
 
         $.getJSON('assets/json/countryBorders.geo.json', function (geoJSONdata) {
-            var selectedCountry = getCountryFromCoordinates(latitude, longitude, geoJSONdata);
-            console.log(selectedCountryName);
-        
-        console.log(getCountryFromCoordinates(latitude, longitude, geoJSONdata));
 
-        // Get the country name by the current location
-        selectedCountryName = getCountryFromCoordinates(latitude, longitude, geoJSONdata);
+            var selectedCountry = getCountryFromCoordinates(latitude, longitude, geoJSONdata);
+            var selectedCountryCode = getCountryCodeFromCoordinates(latitude, longitude, geoJSONdata);
+
+            // Get the country name by the current location
+            selectedCountryName = getCountryFromCoordinates(latitude, longitude, geoJSONdata);
+
+            map.eachLayer(function (layer) {
+                if (layer !== featureGroup && layer !== mapLayer) {
+                    map.removeLayer(layer);
+                }
+            });
+
+            featureGroup.eachLayer(function (layer) {
+                if (layer.feature.properties.name === selectedCountry) {
+                    layer.setStyle({ className: 'selected' });
+                    map.addLayer(layer);
+                } else {
+                    layer.setStyle({ className: '' });
+                }
+
+            });
+
+            var selectedLayer = featureGroup.getLayers().find(function (layer) {
+                return layer.feature.properties.name === selectedCountry;
+            });
+            if (selectedLayer) {
+                map.fitBounds(selectedLayer.getBounds());
+            }
+
+            map.addLayer(mapLayer);
+
+            if (selectedCountryName !== '') {
+                var selectedLayer = featureGroup.getLayers().find(function (layer) {
+                    return layer.feature.properties.name === selectedCountryName;
+                });
+
+                if (selectedLayer) {
+                    map.fitBounds(selectedLayer.getBounds());
+                }
+            }
+
         });
     });
 } else {
@@ -113,16 +248,10 @@ if ("geolocation" in navigator) {
 $(document).ready(function () {
     var selectElement = $('#countriesList select');
 
-    // Fazer a requisição fetch para obter os dados do arquivo PHP
     $.getJSON('assets/php/getCountryList.php', function (data) {
-
-        // var option = $('<option>').attr('value', '').text('Countries list');
-        // selectElement.append(option);
-
-        // Iterar pelos dados e criar as opções do <select>
         $.each(data, function (index, country) {
             var option = $('<option>').val(country.iso_a2).text(country.name);
-            
+
             if (country.name === selectedCountryName) {
                 option.attr('selected', 'selected');
             }
@@ -134,16 +263,17 @@ $(document).ready(function () {
 
 //! 
 $('#countriesList select').change(function () {
-    var selectedCountry = $(this).find('option:selected').text();
+    var selectedCountryCodeList = $(this).find('option:selected').val();
+    var selectedCountryList = $(this).find('option:selected').text();
 
     map.eachLayer(function (layer) {
         if (layer !== featureGroup && layer !== mapLayer) {
             map.removeLayer(layer);
         }
     });
-    
+
     featureGroup.eachLayer(function (layer) {
-        if (layer.feature.properties.name === selectedCountry) {
+        if (layer.feature.properties.name === selectedCountryList) {
             layer.setStyle({ className: 'selected' });
             map.addLayer(layer);
         } else {
@@ -153,7 +283,7 @@ $('#countriesList select').change(function () {
     });
 
     var selectedLayer = featureGroup.getLayers().find(function (layer) {
-        return layer.feature.properties.name === selectedCountry;
+        return layer.feature.properties.name === selectedCountryList;
     });
     if (selectedLayer) {
         map.fitBounds(selectedLayer.getBounds());
---
+++ project1/assets/php/getWeatherInfo.php
@@ -0,0 +1,32 @@
+<?php
+
+$weatherstack = '415f51eb6e8613d2309a674a1beadfce';
+
+
+    ini_set('display_errors', 'On');
+    error_reporting(E_ALL);
+
+    $executionStartTime = microtime(true);
+
+    $url='http://api.weatherstack.com/forecast?access_key=' . $weatherstack . '&query=' . $_REQUEST['adminName1'];
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_URL,$url);
+
+    $result=curl_exec($ch);
+
+    curl_close($ch);
+
+    $decode = json_decode($result,true);
+
+    $output = $decode;
+    $output['status']['code'] = "200";
+    $output['status']['name'] = "ok";
+    $output['status']['description'] = "success";
+    $output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
+    
+    header('Content-Type: application/json; charset=UTF-8');
+
+    echo json_encode($output); 
